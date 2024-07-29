02d22c53434640cc455c83e32e0bce031c7e3219
Akumar <128833380+Adityaakr@users.noreply.github.com>
2024-07-29 18:36:36 +0530

    Update HomeTopBar.tsx

--- src/components/home/HomeTopBar.tsx
+++ src/components/home/HomeTopBar.tsx
@@ -11,11 +11,11 @@ export const HomeTopBar: FC = () => {
         tw="absolute top-0 left-0 right-0 z-10 flex items-center justify-center whitespace-pre-wrap bg-gray-900 py-3 px-2 text-center font-semibold text-sm text-white/75 hover:text-white"
       >
         <div tw="mr-2 rounded px-1.5 py-0.5 font-bold text-xs text-white bg-[#ee391c]">
-          <span tw="mr-1 hidden sm:inline">VIDEO</span>▶
+          <span tw="mr-1 hidden sm:inline"></span>▶
         </div>
         <div tw="font-bold">
-          <span tw="hidden sm:inline">Watch the sub0 ink!athon workshop (45 min)</span>
-          <span tw="inline sm:hidden">sub0 ink!athon workshop</span>
+          <span tw="hidden sm:inline"></span>
+          <span tw="inline sm:hidden"></span>
         </div>
         <HiOutlineExternalLink tw="ml-1.5" />
       </Link>
